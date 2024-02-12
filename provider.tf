1e276df6cea3560b5e38a9ee606557d656eaf6f9
Dhruvin Soni <dhruvinsoni@Dhruvins-MacBook-Pro.local>
2024-02-12 19:18:22 +0530

    Updating provider file

--- provider.tf
+++ provider.tf
@@ -1,16 +1,14 @@
 # configure aws provider
 provider "aws" {
   region  = var.region
-  profile = "dhsoni"
 }
 
 # configure backend
 terraform {
   backend "s3" {
     bucket         = "dhsoni-terraform"
-    key            = "terraform.tfstate"
+    key            = "eks.terraform.tfstate"
     region         = "us-east-2"
-    profile        = "dhsoni"
     dynamodb_table = "terraform-state-lock-dynamodb"
   }
 }
