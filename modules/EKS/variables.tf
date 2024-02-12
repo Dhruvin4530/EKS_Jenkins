0f567c38a8e504d0668bc0857dfa85959d1bab1b
Dhruvin Soni <dhruvinsoni@Dhruvins-MacBook-Pro.local>
2024-02-12 16:42:15 +0530

    Adding files

---
+++ modules/EKS/variables.tf
@@ -0,0 +1,65 @@
+# Environment
+variable "env" {
+  type = string
+  description = "Environment"
+}
+
+# Type
+variable "type" {
+  type = string
+  description = "Type"
+}
+
+# Stack name
+variable "cluster_name" {
+  type = string
+  description = "Project Name"
+}
+
+# Public subnet AZ1
+variable "public_subnet_az1_id" {
+  type = string
+  description = "ID of Public Subnet in AZ1"
+}
+
+# Public subnet AZ2
+variable "public_subnet_az2_id" {
+  type = string
+  description = "ID of Public Subnet in AZ2"
+}
+
+# Security Group 
+variable "eks_security_group_id" {
+  type = string
+  description = "ID of EKS worker node's security group"
+}
+
+# Master ARN
+variable "master_arn" {
+  type = string
+  description = "ARN of master node"
+}
+
+# Worker ARN
+variable "worker_arn" {
+  type = string
+  description = "ARN of worker node"
+}
+
+# Key name
+variable "key_name" {
+  type = string
+  description = "Name of SSH key"
+}
+
+# Worker Node & Kubectl instance size
+variable "instance_size" {
+  type = string
+  description = "Worker node's instance size"
+}
+
+# node count
+variable "count" {
+  type = string
+  description = "Worker node's count"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+# UI Access
+http_access = ["0.0.0.0/0"]
+
+# SSH Access
+ssh_access = ["0.0.0.0/0"]
